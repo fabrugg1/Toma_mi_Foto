f5c0ef1b612a89dc9c28ecbed5f45debddf9fbd1
fabrugg1 <131212018+fabrugg1@users.noreply.github.com>
2023-04-20 16:37:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,62 +1,87 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-  
+
 var recognition = new SpeechRecognition();
+//agrega esta línea para que reconozca el español
+recognition.lang="es-MX";
 
-function start()
+var Textbox = document.getElementById("textbox");
+
+function start() 
 {
-    document.getElementById("textbox").innerHTML = ""; 
+    Textbox.innerHTML = "";
     recognition.start();
-} 
- 
-recognition.onresult = function(event) {
+}
 
-     console.log(event); 
+recognition.onresult = function (event) 
+{    
+    console.log(event);
 
     var Content = event.results[0][0].transcript;
 
-    document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-
-    if(Content == "se toma mi foto"){
+    Textbox.innerHTML = Content;
+    //Primero, añade el código para verificar que se dice “toma una selfie”  en ella llama a la función speak (solo es moverla)
+    if ( Content=="Toma mi selfie")
+    {
+        console.log("tomando selfie ---");
         speak();
     }
-
-        
+    
 }
 
-
-function speak(){
+function speak (){
+   
     var synth = window.speechSynthesis;
 
-    //speak_data = document.getElementById("textbox").innerHTML;
-    speak_data = "che tomando tu foto en 5 segundos";
+    //Vamos a remplazar speak_data=document.getElementById("textbox").value; para que diga "Toma la selfie en 5 segundos"
+    speak_data="Tomando tu Selfie en 5 segundos "
 
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    var utterThis = new SpeechSynthesisUtterance(speak_data); 
 
     synth.speak(utterThis);
 
     Webcam.attach(camera);
 
-    setTimeout(function() { take_selfie(); save(); }, 5000)
-}
+    //vamos a definir la función set Timeout dentro de la función speak() 
 
-camera = document.getElementById("camera");
+    setTimeout(function()
+    {
+        take_selfie();
+        //llamar a la función save
+        save();
+
+    }, 5000);//tiempo en milisegundos
+
+}
+//Agregar la variable Camera
+camera=document.getElementById("camera");
+
 Webcam.set({
+   
     width:360,
     height:250,
-    image_format : 'jpeg',
-    jpeg_quality:90
+    image_format:"png",
+    png_quality:90
 });
 
-function take_selfie() {
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+
+//Agrega la función take_selfie
+function take_selfie()
+{
+    //data_url se usa para visualizar la imagen.
+    Webcam.snap(function(data_url){
+
+        //Vamos a actualizar el div que hicimos para que contenga la imagen
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_url+'"/>';
     });
 }
 
-function save() {
-    link = document.getElementById("link");
-    image = document.getElementById("selfie_image").src;
-    link.href = image;
+//Declaramos la función save
+function save()
+{
+    link=document.getElementById("link");
+    //image es la variable en la que almacenaremos la imagen
+    image=document.getElementById("selfie_image").src;
+    link.href=image
     link.click();
 }
